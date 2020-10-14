c9117e58ac8cb4ea9d7739ce39942dbfabe5b74b
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-10-14 16:46:38 -0300

    trabalahndo com rotas, criação da pagina orphanagesMap com a sidebar estilizada.

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
-    return <h1>Hello World</h1>
+    return (
+            <div id="page-map">
+                <aside>
+                    <header>
+                        <img src={mapMarkerImg} alt="Happy"/>
+
+                        <h2>Escolha um orfonato no mapa</h2>
+                        <p>Muitas Crianças estão esperando a sua visita</p>
+                    </header>
+
+                    <footer>
+                        <strong>
+                            São Paulo
+                        </strong>
+                        <span> São Paulo</span>
+                    </footer>
+                </aside>
+
+            <div></div>
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"> </FiPlus>
+            </Link>
+
+            </div>
+    ) 
 }
 
 export default OrphanagesMap;
