b7ffbd4a35649aab40cf8a8331bb9f8e5238a62e
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-10-14 20:15:27 -0300

    terminado o primeriro dia add mapa do site Mapbox e openstreetmap.

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import {Map,TileLayer} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -13,7 +16,7 @@ function OrphanagesMap() {
                     <header>
                         <img src={mapMarkerImg} alt="Happy"/>
 
-                        <h2>Escolha um orfonato no mapa</h2>
+                        <h2>Escolha um orfanato no mapa</h2>
                         <p>Muitas Crianças estão esperando a sua visita</p>
                     </header>
 
@@ -25,7 +28,19 @@ function OrphanagesMap() {
                     </footer>
                 </aside>
 
-            <div></div>
+            <Map
+                center={[-23.7351067,-46.7607896]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* mapa do openstreetmap */}
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />     */}
+                <TileLayer 
+                    // mapa do MapBox litgh-v10
+                url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF"> </FiPlus>
             </Link>
