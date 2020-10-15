d59cd5011fb20e6981049e8627615d071e13b52e
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-10-15 20:38:36 -0300

    Começo da criação do Backend em Node, com a criação dos qruivos de Banco de Dados.

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app =  express();
+
+app.use(express.json());
+
+
+// Query Params: http://localhost:3333/users?search=john
+// Route params: http://localhost:3333/users?/1 (identificar recurso)
+// Body: http://localhost:3333/users (identificar um recurso)
+
+app.get('/users',(request, response)=>{  
+    return response.json( { message: 'Helo World'});
+    
+});
+
+
+app.listen(3333);
+
+
