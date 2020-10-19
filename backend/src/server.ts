545f317708aee7d8edf27c26e7864cc70c1690ea
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-10-19 11:08:03 -0300

    abstraindo o codigo com Controller e criação de rotas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,47 +1,15 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app =  express();
 
 app.use(express.json());
 
+app.use(routes);
 
-// Query Params: http://localhost:3333/users?search=john
-// Route params: http://localhost:3333/users?/1 (identificar recurso)
-// Body: http://localhost:3333/users (identificar um recurso)
 
-app.post('/orphanages', async (request, response)=>{  
-
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        opening_on_weekends,
-    } = request.body;
-    
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        opening_on_weekends,
-    });
-
-     await orphanagesRepository.save(orphanage);
-
-    return response.json( { message: 'Helo World' } );
-    
-});
 
 
 app.listen(3333);
