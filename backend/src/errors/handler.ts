51d5ebff52a57c3822b3ba065ccdd144a71c0e3f
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-10-19 22:16:08 -0300

    terminado otratamento de exceções dos errors.

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,5 +1,9 @@
 import { ErrorRequestHandler } from 'express';
 
-const errorHandler = () => {
+const errorHandler: ErrorRequestHandler = (error, request, response) => {
+    console.error(error);
 
+    return response.status(500).json({ message: 'Internal server error'});
 };
+
+export default errorHandler;
