9891450b8b923c9faf252dc89426049983f5dcff
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-12-29 19:35:39 -0300

    e http no imagem_view e nao https.

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image) { 
         return {
             id: image.id,
-            url: `https://192.168.0.108:3333/uploads/${image.path}`,
+            url: `http://192.168.0.108:3333/uploads/${image.path}`,
         };
     },
     renderMany(images: Image[]) {
